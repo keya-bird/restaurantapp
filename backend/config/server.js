52d90d2ec6f34d0251349b5ac65f3dc8994e260e
keya-bird <56005385+keya-bird@users.noreply.github.com>
2024-06-09 10:11:51 -0600

    Update server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,27 +1,10 @@
-const { env } = require('path');
-
 module.exports = ({ env }) => ({
-  // **Database Connection Configuration**
-  connection: {
-    client: env('DATABASE_CLIENT', 'postgres'),
-    connection: {
-      url: env('postgres://postgres2024:sw6tCa2ejpJkFbn4Em5erkZuXXtq7mik@dpg-cphjfc0cmk4c73ej56eg-a.oregon-postgres.render.com/nextapp_2703'),
-    },
-    pool: {
-      min: 0,
-      max: 10,
-    },
-  },
-
-  // **App Keys Configuration**
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', []), // Set a default empty array to avoid errors
+    keys: env.array('APP_KEYS'),
   },
-
-
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-
-
 });
