5114053d91e0a925254b03ffe51ae86193656c65
keya-bird <56005385+keya-bird@users.noreply.github.com>
2024-06-08 21:24:30 -0600

    Update server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,13 +1,16 @@
 const { env } = require('strapi');
 
+module.exports = ({ env }) => ({
 connection: {
   client: env('DATABASE_CLIENT', 'postgres'),
   connection: {
     url: env('postgres://postgres2024:sw6tCa2ejpJkFbn4Em5erkZuXXtq7mik@{{postgres_host}}/nextapp_2703?sslmode=require'), // Use placeholder and add sslmode
   },
+  app: {
+    keys: env.array('APP_KEYS'),
   pool: {
     min: 0,
     max: 10,
   },
 },
-
+});
