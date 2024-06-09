d8650778476ef5f78ab5906d16e0c926151ce911
keya-bird <56005385+keya-bird@users.noreply.github.com>
2024-06-09 11:00:59 -0600

    Create plugins.js

---
+++ backend/app/config/plugins.js
@@ -0,0 +1,17 @@
+import graphql from "@strapi/plugin-graphql/strapi-admin";
+import i18N from "@strapi/plugin-i18n/strapi-admin";
+import upload from "@strapi/plugin-upload/strapi-admin";
+import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
+import { renderAdmin } from "@strapi/strapi/admin";
+
+renderAdmin(document.getElementById("strapi"), {
+  plugins: {
+    graphql: graphql,
+    i18n: i18N,
+    upload: upload,
+    "users-permissions": usersPermissions,
+    config: {
+      jwtSecret: env('xMKu3WninpFtH0fK/NXTcg=='),
+  },
+},
+});
