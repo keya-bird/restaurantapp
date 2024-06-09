dea6b3efcf942142d6e3a889c2222528ab9b92dd
keya-bird <56005385+keya-bird@users.noreply.github.com>
2024-06-08 21:36:55 -0600

    Update server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -5,12 +5,13 @@ connection: {
   client: env('DATABASE_CLIENT', 'postgres'),
   connection: {
     url: env('postgres://postgres2024:sw6tCa2ejpJkFbn4Em5erkZuXXtq7mik@{{postgres_host}}/nextapp_2703?sslmode=require'), // Use placeholder and add sslmode
-  },
   app: {
     keys: env.array('APP_KEYS'),
   pool: {
     min: 0,
     max: 10,
   },
-},
+}
+  }
+}
 });
