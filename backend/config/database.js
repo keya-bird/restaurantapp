2e042e431bdcbc113a4e7a735b230bb16487d8d7
keya-bird <56005385+keya-bird@users.noreply.github.com>
2024-06-08 22:31:32 -0600

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,83 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres', // Use 'pg' for PostgreSQL
-    connection: {
-      host: 'dpg-cphjfc0cmk4c73ej56eg-a', // Replace with your actual Render PostgreSQL hostname
-      port: 5432, // Default PostgreSQL port
-      database: 'nextapp_2703', // Replace with your database name
-      user: 'postgres2024', // Replace with your PostgreSQL username
-      password: 'sw6tCa2ejpJkFbn4Em5erkZuXXtq7mik', // Replace with your PostgreSQL password
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);//Getting the URL from the Environment
+
+module.exports = ({ env }) => {
+  const client = env("DATABASE_CLIENT", "postgres");
+
+  const connections = {
+    mysql: {
+      connection: {
+        connectionString: env("DATABASE_URL"),
+        host: env("DATABASE_HOST", "dpg-cphjfc0cmk4c73ej56eg-a"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "nextapp_2703"),
+        user: env("DATABASE_USERNAME", "postgres2024"),
+        password: env("DATABASE_PASSWORD", "sw6tCa2ejpJkFbn4Em5erkZuXXtq7mik"),
+        ssl: env.bool("DATABASE_SSL", false) && {
+          key: env("DATABASE_SSL_KEY", undefined),
+          cert: env("DATABASE_SSL_CERT", undefined),
+          ca: env("DATABASE_SSL_CA", undefined),
+          capath: env("DATABASE_SSL_CAPATH", undefined),
+          cipher: env("DATABASE_SSL_CIPHER", undefined),
+          rejectUnauthorized: env.bool(
+            "DATABASE_SSL_REJECT_UNAUTHORIZED",
+            true
+          ),
+        },
+      },
+      pool: {
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
+      },
     },
-    useNullAsDefault: true,
-  },
-});
+    postgres: {
+      connection: {
+        connectionString: env("DATABASE_URL"),//DESTRUCTURING THE URL
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: env.bool("DATABASE_SSL", false) && {
+          key: env("DATABASE_SSL_KEY", undefined),
+          cert: env("DATABASE_SSL_CERT", undefined),
+          ca: env("DATABASE_SSL_CA", undefined),
+          capath: env("DATABASE_SSL_CAPATH", undefined),
+          cipher: env(
+            "DATABASE_SSL_CIPHER",
+            "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+          ),
+          rejectUnauthorized: env.bool(
+            "DATABASE_SSL_REJECT_UNAUTHORIZED",
+            true
+          ),
+        },
+        schema: env("DATABASE_SCHEMA", "public"),
+      },
+      pool: {
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
+      },
+    },
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          env("DATABASE_FILENAME", "data.db")
+        ),
+      },
+      useNullAsDefault: true,
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+    },
+  };
+};
