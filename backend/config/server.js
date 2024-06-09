1c1761aed04151a2a51f5fe21c8dc2638b2cdbb9
keya-bird <56005385+keya-bird@users.noreply.github.com>
2024-06-08 22:04:12 -0600

    Update server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,16 +1,11 @@
 module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-connection: {
-  client: env('DATABASE_CLIENT', 'postgres'),
-  connection: {
-    url: env('postgres://postgres2024:sw6tCa2ejpJkFbn4Em5erkZuXXtq7mik@{{postgres_host}}/nextapp_2703?sslmode=require'), // Use placeholder and add sslmode
   app: {
-    keys: env.array(["aW2olD4It8bifkvZIU2u+g==", "QJhqnaA00qbYAhI20SjxJw==", "zWOcDmYfk5qURWd9NOLldw==", "3to5BodorPBGvtj4qD+hdA=="]),
-  pool: {
-    min: 0,
-    max: 10,
+    keys: env.array('APP_KEYS',['aW2olD4It8bifkvZIU2u+g==','QJhqnaA00qbYAhI20SjxJw==','zWOcDmYfk5qURWd9NOLldw==','3to5BodorPBGvtj4qD+hdA==']),
   },
-}
-  }
-}
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
 });
+
