097f869428f8b4b5f84b05f78b06253fae44a481
keya-bird <56005385+keya-bird@users.noreply.github.com>
2024-06-08 20:28:52 -0600

    Update server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,11 +1,19 @@
+const { env } = require('strapi');
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array(['APP_KEYS','aW2olD4It8bifkvZIU2u+g==','QJhqnaA00qbYAhI20SjxJw==','zWOcDmYfk5qURWd9NOLldw==','3to5BodorPBGvtj4qD+hdA==']),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  // ... other configurations
+  database: {
+    connection: {
+      client: env('DATABASE_CLIENT', 'postgres'),
+      connection: {
+        url: env('postgres://default:dYibNMf68uCF@ep-little-rain-a47dzfy6-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require'),
+        user: env('default'),
+        password: env('dYibNMf68uCF'),
+      },
+      pool: {
+        min: 0,
+        max: 10,
+      },
+    },
   },
 });
-
