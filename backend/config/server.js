3020c1b6fad70cee55a07fadf30a97156749aa8d
keya-bird <56005385+keya-bird@users.noreply.github.com>
2024-06-08 21:17:49 -0600

    Update server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,20 +1,13 @@
 const { env } = require('strapi');
 
-module.exports = ({ env }) => ({
-  host: env('postgres_host', 'ep-little-rain-a47dzfy6-pooler.us-east-1.aws.neon.tech'),
-  port: env.int('PORT', 1337),
-  database: {
-    connection: {
-      client: env('DATABASE_CLIENT', 'postgres'),
-      connection: {
-        url: env('postgres://default:dYibNMf68uCF@ep-little-rain-a47dzfy6-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require'),
-        user: env('default'),
-        password: env('dYibNMf68uCF'),
-      },
-      pool: {
-        min: 0,
-        max: 10,
-      },
-    },
+connection: {
+  client: env('DATABASE_CLIENT', 'postgres'),
+  connection: {
+    url: env('postgres://postgres2024:sw6tCa2ejpJkFbn4Em5erkZuXXtq7mik@{{postgres_host}}/nextapp_2703?sslmode=require'), // Use placeholder and add sslmode
   },
-});
+  pool: {
+    min: 0,
+    max: 10,
+  },
+},
+
