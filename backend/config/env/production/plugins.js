eeaa2b5943203f1fa9c66ef4bdf1abe51fdd4cee
keya-bird <56005385+keya-bird@users.noreply.github.com>
2024-06-12 16:28:30 -0600

    Update plugins.js
    changed securtiy

--- backend/config/env/production/plugins.js
+++ backend/config/env/production/plugins.js
@@ -3,7 +3,7 @@ module.exports = {
       config: {
         endpoint: "/graphql",
         shadowCRUD: true,
-        playgroundAlways: true,
+        playgroundAlways: false,
         depthLimit: 10,
         amountLimit: 100,
         apolloServer: {
